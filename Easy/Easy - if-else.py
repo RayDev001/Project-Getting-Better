6d98a6a80133b80c6ecaecd46f4996fe7dc8db81
RayDev001 <raynickrosario@alumno.utesa.edu>
2026-09-13 17:49:29 -0400

    Fixed "Weeird" typo
    The original code had "Weeird" and the test cases were failing.
    I corrected it by spelling the word correctly.

--- Easy/Easy - if-else.py
+++ Easy/Easy - if-else.py
@@ -22,7 +22,7 @@ def isWeird(n: int) -> None:
         if n in range(2, 5 + 1):
             print("Not Weird")
         elif n in range(6, 20 + 1):
-            print("Weeird")
+            print("Weird")
         elif n > 20:
             print("Not Weird")
     else:
